e6414ba6b7d6bc8748becb91fafc7d0e465d05c5
Alex <alex@sitepoint.com>
2018-01-12 16:00:18 +1100

    Covers generated by a single command

--- src/docs/markade.js
+++ src/docs/markade.js
@@ -1,7 +1,7 @@
 module.exports = {
   "name": "perf4",
-  "booktitle": '<span>Performance</span> Tools',
-  "booktitleClean": 'Performance Tools',
+  "booktitle": '<span>Performance</span> Woody',
+  "booktitleClean": 'Performance Woody',
   "tagline": 'Because Every Bottleneck Costs You <em>Real</em> Money',
   "backpitch": 'What Happens When You Remove Speed Humps For Your Users?',
   "pagenumbers": '150',
@@ -9,7 +9,7 @@ module.exports = {
   "authorPitch": "Each SitePoint Anthology is a carefully curated collection of the best SitePoint writing around a chosen topic. That means you can be sure each chapter industry relevant, up-to-date and written by a world expert in their field. We know you'll enjoy every page.",
   "subject": 'Performance',
   "category": 'Web Development',
-  "colorkey": '#3EC7A8',
+  "colorkey": '#C7A83E',
   "colorsecondary": '#222244',
   "textcolorlight": '#fff',
   "textcolordark": '#222244',
---
+++ src/docs/md/book-head.md
@@ -0,0 +1,8 @@
+---
+template: book-head.pug
+---
+
+
+[//]: # (Feel free to edit any of the values above or MD below.)
+[//]: # (You changes will be included in the new Markade generated HTML)
+
